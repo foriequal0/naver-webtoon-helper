f8c5f4b54435bc259f9bfc16f4b2cdf87205e99f
SeongChan Lee <foriequal@gmail.com>
2023-04-09 14:26:41 +0900

    요일별 보기 정렬 안되던거 수정

--- src/page/weekdayList.ts
+++ src/page/weekdayList.ts
@@ -2,6 +2,6 @@ import { rearrange } from "../rearrange";
 import { querySelector } from "../selectors";
 
 export async function weekdayList() {
-  const list = await querySelector('li[class^="ContentList__content_list"]');
+  const list = await querySelector('ul[class^="ContentList__content_list"]');
   await rearrange("webtoon", list);
 }
